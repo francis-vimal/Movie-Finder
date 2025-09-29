8d6e28c01a1559f4feb2265e54df4bc9b3b4c029
francisvimal2002 <francisvimal2002@gmail.com>
2025-09-29 13:04:39 +0000

    002 - EJS assignment, added functionality for rating and genre chips

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import axios from 'axios'
 import bodyParser from "body-parser";
 import mockData from "./data/static.json" assert { type: "json" };
+import genreData from "./data/genre.json" assert { type: "json" };
 
 const app = express();
 const port = 3000;
@@ -12,7 +13,11 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    mockData.release_date = dateConversion(mockData.release_date);
+    const displayNames = new Intl.DisplayNames(['en'], { type: 'language' });
+    mockData.original_language = displayNames.of(mockData.original_language) + ' (' + mockData.original_language + ')';
+    // console.log(displayNames.of(mockData.original_language)); 
+    res.render('index.ejs', { movieData: mockData, genreData: genreData});
 })
 
 app.get('/search', async (req, res) => {
@@ -28,14 +33,20 @@ app.get('/search', async (req, res) => {
                 'X-RapidAPI-Host': 'ai-movie-recommender.p.rapidapi.com'
             }
         })
-        console.data(response.data);
-        res.render('index.ejs', { movieData: response.data});
+        console.log(response.data);
+        res.render('index.ejs', { movieData: response.data, genreData: genreData});
 
     } catch (error) {
         console.error(error.message);
         res.render('index.ejs', { movieData: mockData});
     }
 })
+
+function dateConversion(date) {
+    const dateObj = new Date(date);
+    const options = { day: "2-digit", month: "short", year: "numeric" };
+    return dateObj.toLocaleDateString("en-GB", options);
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port https://localhost:${port}`);
